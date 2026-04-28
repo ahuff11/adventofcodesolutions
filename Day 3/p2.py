883d6b3b9217fdbc5e890d809e16f79fe696bf3c
ahuff11 <huffae2000@gmail.com>
2026-04-27 23:27:00 -0700

    Day 4 pt 1 complete

--- Day 3/p2.py
+++ Day 3/p2.py
@@ -1,5 +1,4 @@
 
-
 def getJoltage() -> int:
     file = "Day 3/input.txt"
     voltage = 0
@@ -33,17 +32,3 @@ if __name__ == "__main__":
     main()
 
 
-
-
-
-# ---------------------------------------BRAINSTORM SLOP-----------------------------------------------------
-# we can use like a heap we could loop through and once we hit size 12 for our jolts we can compare maybe
-# loop through and find 
-# we do a min heapq
-# in the heap we append the leftmost high and save it as left most high  
-# if i +12 = len(line)
-# stack monotonic as long as it passes the condition then we add it 
-# add elements to the stack if stack.size<12, if i+12 == len(line), if line[i] > stack.pop
-# once we get towards the last elements in the stack we need to lock in the elements 
-# what is the condition to where we can't add more numbers
-# we could save a copy of the stack do the pop anyway and if its len(line) - i = how many elements are left. if after pop happens and the len of stack <12 then we go back to our copy and continue 
--- Day 4/p1.py
+++ Day 4/p1.py
@@ -1,25 +1,55 @@
+import collections
 
 
-
-def findrolls() -> int:
+def readInput() -> list[list[str]]:
     file = "Day 4/input.txt"
-    rolls = 0
+    matrix = []
 
     try:
         with open(file, 'r') as file:
             for line in file:  
-                line = line.strip() 
+                matrix.append(list(line.strip()))
 
 
-        return rolls
+        return matrix
     except FileNotFoundError:
         print(f"Error: The file '{file}' was not found.")
         return 0
     
 
-def main():
-    print(findrolls)
+def findRolls(matrix : list[list[str]]) -> int:
+    count = 0
+    q = collections.deque()
+    ROWS, COLS = len(matrix), len(matrix[0])
+    neighbors = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
 
+   # print(matrix[1][1])
+    for r in range(ROWS):
+        for c in range(COLS):
+
+            if matrix[r][c] == "@":
+                county = 0
+                for dr, dc in neighbors:
+                    nr, nc = dr+r, dc+ c
+                    if min(nr, nc) < 0 or nr == ROWS or nc == COLS or matrix[nr][nc] == ".":
+                        continue
+                    county +=1 
+                if county <=3:
+                    count+=1
+    
+    return count
+
+
+
+
+
+def main():
+
+    input = readInput()
+    # for i in input:
+    #     print(i)
+    
+    print(findRolls(input))
 
 if __name__ == "__main__":
     main()
@@ -27,8 +57,11 @@ if __name__ == "__main__":
 
 # Brainstorm slop
 # okay didnt realize this is a grid problem. The eight neighbors is the area that surrounds the grid. 
-# i think a solution could be a bit map. 
-# going to be n^2 no matter 
+# i think a solution could be a bit map.  
 # i will probably have to pass through and save some sort of information then get the answer 
-# holy shit can I use a 2d sliding window
 # we loop through and we store current, topleft, topmid, topright, left, right, bottomleft, bottommid, bottom right
+#so we need to handle edge cases where we are at the top, bottom, lef or right
+# if row == 0, then we dont need to worry about top
+# if row == len(matrix)-1 then we don't need to worry about the bottom
+# if col == 0 we don't need to worry about left, 
+# if col == len(row)-1 we don't need to worry about right
